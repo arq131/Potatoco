66be791aa8439a45e9ebe85ea95fc33696b351e8
Ricardo <ricarles11@gmail.com>
2018-03-23 21:03:26 -0500

    3 products in toal so far

--- main/Products/Descriptions.java
+++ main/Products/Descriptions.java
@@ -21,5 +21,14 @@ public class Descriptions {
         pics[0] = "main\\PICS\\ball.png";
         descriptions[0] = "If you want to know the answer to questions like: 'will I get that job as a potato farmer'\n" +
                 " or 'Does she like sweet potatoes?' well the 8-potato fortune teller is here";
+        names[1] = "Pugtato shirt";
+        pics[1] = "main\\PICS\\pugtato_shirt.jpg";
+        descriptions[1] = "When you love pugs but you also love potatoes here is a\n" +
+                "shirt that expresses your love for both";
+        names[2] = "Potatogram";
+        pics[2] = "main\\PICS\\Potatogram.jpg";
+        descriptions[2] = "Have you ever wanted to send a message to someone in a meaningful way but mail,\n" +
+                " email and text messaging feels too bland and without enough potatos? well here comes the\n" +
+                " Potatogram. Personalize your message and show your love for potatos at the same time";
     }
 }
