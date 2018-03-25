47d9245473e560ddcc4b5d12d4ac27f978d8cc69
Jason <Jason@DESKTOP-5MOISC7>
2018-03-25 15:17:27 -0500

    Main launcher for launcher screen

---
+++ main/app/Launcher.java
@@ -0,0 +1,35 @@
+package app;
+
+import javafx.application.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.*;
+import java.net.URL;
+import controller.AppController;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+public class Launcher extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		AppController appControl = AppController.getInstance();
+		URL res = this.getClass().getResource("/view/LaunchScreen.fxml");
+		FXMLLoader loader = new FXMLLoader(res);
+		loader.setController(appControl);
+		Parent root = loader.load();
+		appControl.setRootPane((BorderPane)root);
+		
+		Scene scene = new Scene(root, 1024, 768);
+		primaryStage.setTitle("Welcome to Potaco");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
